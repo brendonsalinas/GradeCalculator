bbc44a59741cc1defb0f3f148ef4978b5ff8a549
Brendon Salinas <brendonsalinas@brendons-mbp.gateway.pace.com>
2014-12-22 18:10:31 -0800

    In progress

---
+++ app/src/main/java/com/lazyengineers/gradecalculator/year1.java
@@ -0,0 +1,49 @@
+package com.lazyengineers.gradecalculator;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class year1 extends ActionBarActivity {
+
+    Boolean newOne = true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_year1);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_year1, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void newClass(View view) {
+        final int result = 1;
+        Intent getClassIntent = new Intent(this, NewClass.class);
+        startActivityForResult(getClassIntent,result);
+    }
+}
